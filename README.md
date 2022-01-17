aa934caa7c7a3393601e97b2f6e0daa8f2a62295
benjyms <87244593+benjyms@users.noreply.github.com>
2022-01-17 16:34:09 -0500

    readme, notebook and sql code updates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # big-data-challenge
+
 CWRU Bootcamp Big Data Homework
+
+Pyspark query 1 created for amazon watches reviews.  See Notebook ()
+
+AWS DB Created
+
+SQL queries used to create tables:
+
+![](image/README/1642449978085.png)
+
+![](image/README/1642453934216.png)
---
+++ level-1/temp.sql
@@ -0,0 +1,58 @@
+-- CREATE TABLE watch_review_id (
+--   review_id TEXT PRIMARY KEY NOT NULL,
+--   customer_id INTEGER,
+--   product_id TEXT,
+--   product_parent INTEGER,
+--   review_date DATE -- this should be in the formate yyyy-mm-dd
+-- );
+
+-- -- This table will contain only unique values
+-- CREATE TABLE watch_products (
+--   product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+--   product_title TEXT
+-- );
+
+-- -- Customer table for first data set
+-- CREATE TABLE watch_customers (
+--   customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+--   customer_count INT
+-- );
+
+-- -- vine table
+-- CREATE TABLE watch_vine (
+--   review_id TEXT PRIMARY KEY,
+--   star_rating INTEGER,
+--   helpful_votes INTEGER,
+--   total_votes INTEGER,
+--   vine TEXT
+-- );
+-- #########################################################################
+
+-- CREATE TABLE gc_review_id (
+--   review_id TEXT PRIMARY KEY NOT NULL,
+--   customer_id INTEGER,
+--   product_id TEXT,
+--   product_parent INTEGER,
+--   review_date DATE -- this should be in the formate yyyy-mm-dd
+-- );
+
+-- -- This table will contain only unique values
+-- CREATE TABLE gc_products (
+--   product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+--   product_title TEXT
+-- );
+
+-- -- Customer table for first data set
+-- CREATE TABLE gc_customers (
+--   customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+--   customer_count INT
+-- );
+
+-- -- vine table
+-- CREATE TABLE gc_vine (
+--   review_id TEXT PRIMARY KEY,
+--   star_rating INTEGER,
+--   helpful_votes INTEGER,
+--   total_votes INTEGER,
+--   vine TEXT
+-- );
